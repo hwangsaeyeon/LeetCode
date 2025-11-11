099be3533f1b9ab225cacbbc1518a9f027d0310b
Saeyeon Hwang <87689529+hwangsaeyeon@users.noreply.github.com>
2025-11-11 15:33:13 +0900

    Time: 0 ms (100%), Space: 10.7 MB (22.4%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size(); 
+        if (n==1)
+            return nums[0]; 
+
+        vector<int> dp(n); 
+
+        dp[0] = nums[0]; 
+
+        for (int i=1; i<n ; i++){
+            if (i==1){
+                dp[i] = max(dp[i-1], nums[i]); 
+            }
+            else{
+                dp[i] = max(nums[i] + dp[i-2], dp[i-1]); 
+            }
+            
+        }
+
+        return max(dp[n-1], dp[n-2]);
+    }
+};
